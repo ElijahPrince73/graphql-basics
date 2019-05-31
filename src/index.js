df02d708ccee7c68963d89defe1005e97e3c434d
Elijah Prince <elijahprince73@gmail.com>
2019-05-31 00:11:50 -0700

    relational data

--- src/index.js
+++ src/index.js
@@ -29,19 +29,46 @@ const posts = [
         id: '1',
         title: 'A new hope',
         body: 'A good moview for sure',
-        published: 1978
+        published: 1978,
+        author: '1',
+        comment: '1234' 
     },
     {
         id: '2',
         title: 'Jedi remix',
         body: 'A good moview for sure',
-        published: 1979
+        published: 1979,
+        author: '2',
+        comment: '555'
     },
     {
         id: '1',
         title: 'Clone Wars',
         body: 'A good moview for sure',
-        published: 2005
+        published: 2005,
+        author: '3',
+        comment: '1255'
+    },
+]
+
+const comments = [
+    {
+        id: '1234',
+        text: 'Just a comment about star wars',
+        author: '1',
+        post: '1'
+    },
+    {
+        id: '555',
+        text: 'Just a comment about star wars the clone wars and how its aight',
+        author: '2',
+        post: '2'
+    },
+    {
+        id: '1255',
+        text: 'Jedi remix not good',
+        author: '3',
+        post: '1'
     },
 ]
 
@@ -51,8 +78,9 @@ const typeDefs = `
     type Query {
         users(query: String): [User]!
         posts(query: String): [Post]!
-        me: User!
         post: Post!
+        comments: [ Comment!]!
+        comment: Comment!
     }
 
     type User {
@@ -60,6 +88,8 @@ const typeDefs = `
         name: String!
         email: String!
         age: Int
+        posts: Post!
+        comments: Comment!
     }
 
     type Post {
@@ -67,6 +97,15 @@ const typeDefs = `
         title: String,
         body: String,
         published: Boolean
+        author: User!
+        comments: Comment!
+    }
+
+    type Comment {
+        id: ID!
+        text: String!
+        author: User!
+        post: Post!
     }
 `
 
@@ -94,14 +133,6 @@ const resolvers = {
                 })
             }
         },
-        me() {
-            return {
-                id: '123',
-                name: 'Named',
-                email: 'test@gmail.com',
-                age: 12
-            }
-        },
         post() {
             return {
                 id: '123',
@@ -109,6 +140,51 @@ const resolvers = {
                 body: 'Star wars leaked',
                 published: true
             }
+        },
+        comments() {
+            return comments
+        }
+    },
+    Post: {
+        author(parent, args, ctx, info) {
+            return users.find((user) => {
+                return user.id === parent.author
+            })
+        }
+    },
+    Post: {
+        comments(parent) {
+            return comments.find((comment) => {
+                return comment.id === parent.comment
+            })
+        }
+    },
+    User: {
+        posts(parent, args, ctx) {
+            return posts.find((post) => {
+                return post.author === parent.id
+            })
+        }
+    },
+    User: {
+        comments(parent) {
+            return comments.find((comment) => {
+                return comment.author === parent.id
+            })
+        }
+    },
+    Comment: {
+        author(parent) {
+            return users.find((user) => {
+                return user.id === parent.author
+            })
+        }
+    },
+    Comment: {
+        post(parent) {
+            return posts.find((post) => {
+                return post.id === parent.post
+            })
         }
     }
 }
